a6098fc2f5d4d47dd4833d09f844ff32aff5ffe0
danielsuhcompsci <danielsuh112@yahoo.com>
2022-09-19 23:23:50 -0500

    Adding game logic

--- main.cpp
+++ main.cpp
@@ -27,9 +27,9 @@ void readInputRow(std::string &line, Creature *grid[10][10], int row) {
 void print(Creature *grid[10][10]) {
   for (int row = 0; row < 10; row++) {
     for (int i = 0; i < 10; i++) {
-      if (dynamic_cast<Ant *>(grid[row][i]) != nullptr) {
+      if (is<Ant>(grid[row][i])) {
         std::cout << "a";
-      } else if (dynamic_cast<Beetle *>(grid[row][i]) != nullptr) {
+      } else if (is<Beetle>(grid[row][i])) {
         std::cout << "B";
       } else {
         std::cout << ".";
@@ -39,58 +39,118 @@ void print(Creature *grid[10][10]) {
   }
 }
 
-void playGame(int turns, Creature *grid[10][10]) {
-  for (int turn = 1; turn <= turns; turn++) {
-    for (int column = 0; column < 10; column++) {
-      for (int row = 0; row < 10; row++) {
-      }
+bool isOnGrid(int row, int column) {
+  return (0 <= row && row < 10 && 0 <= column && column < 10) ? true : false;
+}
+
+template <typename T>
+bool is(Creature *ptr) {
+  return (dynamic_cast<T *>(ptr) != nullptr) ? true : false;
+}
+
+template <typename CreatureType>
+void getNearestDistances(
+    Creature *grid[10][10], int distances[4], int row, int column,
+    const std::unordered_map<int, char> &indexToDirection) {
+  // North, negative y direction
+  int rowIndex = row - 1;
+
+  for (int rowIndex = row - 1; rowIndex >= 0; rowIndex--) {
+    if (is<CreatureType>(grid[rowIndex][column])) {
     }
   }
 }
 
-int main() {
-  std::string fileName, line;
+void getFarthestDistance(Creature *grid[10][10]) {}
 
-  //   std::cout << "Please input the file name: " << std::endl;
-  //   std::cin >> fileName;
-  //   std::ifstream input(fileName);
+int getNeighborCount(Creature *grid[10][10], int row, int column) { return 0; }
 
-  std::ifstream input("input.txt");
+void getBreedInfo(Creature *grid[10][10], bool isCreature[4], int row,
+                  int column) {}
 
-  // Exit if file wasn't opened properly
-  if (!input.is_open()) {
-    std::cout << "file not opened properly" << std::endl;
-    return 1;
+void beetlePhase(Creature *grid[10][10],
+                 const std::unordered_map<int, char> &indexToDirection) {
+  for (int column = 0; column < 10; column++) {
+    for (int row = 0; row < 10; row++) {
+      if (is<Beetle>(grid[row][column])) {
+        Beetle *beetlePointer = dynamic_cast<Beetle *>(grid[row][column]);
+
+        int distances[4];
+
+        std::string decisionString =
+            beetlePointer->Move(distances, indexToDirection);
+      }
+    }
   }
 
-  Creature *grid[10][10];
-  const std::unordered_map<int, char> indexToDirection(
-      {{0, 'N'}, {1, 'E'}, {2, 'S'}, {3, 'W'}});
+  void playGame(int turns, Creature *grid[10][10],
+                const std::unordered_map<int, char> &indexToDirection) {
+    for (int turn = 1; turn <= turns; turn++) {
+      for (int phase = 0; phase < 2; phase++) {
+        switch (phase) {
+          case 0:  // Beetles move
+            break;
+          case 1:  // Ants move
+            break;
+          case 2:  // Beetles starve
+            break;
+        }
+      }
 
-  // getline(input, line);
-  // std::cout << line << std::endl;
+      // Ants breed
+      if (turn % 3 == 0) {
+      }
 
-  // Read grid from file
-  for (int row = 0; row < 10; row++) {
-    std::getline(input, line);
-    // std::cout << line;
-    readInputRow(line, grid, row);
+      // Beetles breed
+      if (turn % 8 == 0) {
+      }
+    }
   }
 
-  print(grid);
+  int main() {
+    std::string fileName, line;
 
-  // Ant a;
-  // int distances[4] = {1, 1, 0, 1};  // N, E, S, W
-  // bool isCreature[4] = {true, false, true, false};
-  // std::cout << "Ant move: " << a.Move(distances, indexToDirection) <<
-  // std::endl; std::cout << "Ant breed: " << a.Breed(isCreature,
-  // indexToDirection)
-  //           << std::endl;
+    //   std::cout << "Please input the file name: " << std::endl;
+    //   std::cin >> fileName;
+    //   std::ifstream input(fileName);
 
-  // int beetleDistances[4] = {11, 11, 11, 11};  // N, E, S, W
-  // Beetle b;
-  // std::cout << "Beetle move: " << b.Move(beetleDistances, indexToDirection)
-  //           << std::endl;
-  // std::cout << "Beetle breed: " << b.Breed(isCreature, indexToDirection)
-  //           << std::endl;
-}
+    std::ifstream input("input.txt");
+
+    // Exit if file wasn't opened properly
+    if (!input.is_open()) {
+      std::cout << "file not opened properly" << std::endl;
+      return 1;
+    }
+
+    Creature *grid[10][10];
+    const std::unordered_map<int, char> indexToDirection(
+        {{0, 'N'}, {1, 'E'}, {2, 'S'}, {3, 'W'}});
+
+    // getline(input, line);
+    // std::cout << line << std::endl;
+
+    // Read grid from file
+    for (int row = 0; row < 10; row++) {
+      std::getline(input, line);
+      // std::cout << line;
+      readInputRow(line, grid, row);
+    }
+
+    print(grid);
+
+    // Ant a;
+    // int distances[4] = {1, 1, 0, 1};  // N, E, S, W
+    // bool isCreature[4] = {true, false, true, false};
+    // std::cout << "Ant move: " << a.Move(distances, indexToDirection) <<
+    // std::endl; std::cout << "Ant breed: " << a.Breed(isCreature,
+    // indexToDirection)
+    //           << std::endl;
+
+    // int beetleDistances[4] = {11, 11, 11, 11};  // N, E, S, W
+    // Beetle b;
+    // std::cout << "Beetle move: " << b.Move(beetleDistances,
+    // indexToDirection)
+    //           << std::endl;
+    // std::cout << "Beetle breed: " << b.Breed(isCreature, indexToDirection)
+    //           << std::endl;
+  }
